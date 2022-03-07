90cf6ac04bcf52b286bb66611f12b391799682f1
Adam Borlase <adam.borlase@proquoai.com>
2022-03-07 16:03:57 +0200

    Add Date Filter to Plays Cache

--- notebooks/bggapi.py
+++ notebooks/bggapi.py
@@ -26,8 +26,10 @@ class BGGAPI:
         return final_dict
 
     def get_plays(selfs, game_id: int, cache_location: pathlib.Path):
+
         data_export = cache_location / "data_export.json"
         id_collected_file = cache_location / "id_collected.json"
+        last_date_file = cache_location / "last_date.json"
 
         if id_collected_file.is_file():
             with id_collected_file.open as file:
@@ -37,13 +39,22 @@ class BGGAPI:
             id_collected_json = {}
             id_collected = []
 
+        if last_date_file.is_file():
+            with last_date_file.open() as file:
+                last_date = json.load(file)
+        else:
+            last_date = {}
+
+        if not game_id in last_date:
+            last_date.update({game_id:"0000-00-00"})
+
         if data_export.is_file():
             with data_export.open() as file:
                 final_data = json.load(file)
         else:
             final_data = []
 
-        play_url = f"https://boardgamegeek.com/xmlapi2/plays?id={game_id}"
+        play_url = f"https://boardgamegeek.com/xmlapi2/plays?id={game_id}&mindate={last_date[game_id]}"
 
         url_data = urllib.request.urlopen(play_url).read()
 
@@ -51,7 +62,7 @@ class BGGAPI:
         max_pages = math.ceil(int(soup.plays["total"]) / 100)
 
         for page in tqdm(range(max_pages, 0, -1), maxinterval=max_pages, desc="Page"):
-            url_data = urllib.request.urlopen(f"{play_url}&page={page}").read()
+            url_data = urllib.request.urlopen(f"{play_url}&mindate={last_date[game_id]}&page={page}").read()
             soup2 = BeautifulSoup(url_data, "html.parser")
             for play in soup2.findAll("play"):
                 if not int(play["id"]) in id_collected:
@@ -69,18 +80,26 @@ class BGGAPI:
                     if not play.find("players") == None:
                         player_number = 1
                         for player in play.players.findAll("player"):
-                            dict.update(
-                                {f"player_{player_number}_score": player["score"]}
-                            )
+                            try:
+                                dict.update(
+                                    {f"player_{player_number}_score": int(player["score"])}
+                                )
+                            except:
+                                player_number += 1
+                                pass
                             player_number += 1
                     final_data.append(dict)
+                    last_date.update({game_id: play["date"]})
                     id_collected.append(int(play["id"]))
             sleep(1.5)
             with data_export.open("w") as file:
                 json.dump(final_data, file)
 
             id_collected_json.update({game_id: id_collected})
-
             with id_collected_file.open("w") as file:
                 json.dump(id_collected_json, file)
+
+            with last_date_file.open("w") as file:
+                json.dump(last_date, file)
+
         return final_data
