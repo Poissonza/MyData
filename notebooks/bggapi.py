613ae1bef4737d263e1394555832027e0f673c59
Adam Borlase <adam.borlase@proquoai.com>
2022-03-08 10:44:50 +0200

    Improvements to Plays Fetch

--- notebooks/bggapi.py
+++ notebooks/bggapi.py
@@ -26,15 +26,14 @@ class BGGAPI:
         return final_dict
 
     def get_plays(selfs, game_id: int, cache_location: pathlib.Path):
-
         data_export = cache_location / "data_export.json"
         id_collected_file = cache_location / "id_collected.json"
         last_date_file = cache_location / "last_date.json"
 
         if id_collected_file.is_file():
-            with id_collected_file.open as file:
+            with id_collected_file.open() as file:
                 id_collected_json = json.load(file)
-            id_collected = id_collected_json[game_id]
+            id_collected = id_collected_json[str(game_id)]
         else:
             id_collected_json = {}
             id_collected = []
@@ -45,8 +44,8 @@ class BGGAPI:
         else:
             last_date = {}
 
-        if not game_id in last_date:
-            last_date.update({game_id:"0000-00-00"})
+        if not str(game_id) in last_date:
+            last_date.update({str(game_id): "0000-00-00"})
 
         if data_export.is_file():
             with data_export.open() as file:
@@ -56,22 +55,26 @@ class BGGAPI:
 
         play_url = f"https://boardgamegeek.com/xmlapi2/plays?id={game_id}"
 
-        url_data = urllib.request.urlopen(f"{play_url}&mindate={last_date[game_id]}").read()
+        url_data = urllib.request.urlopen(
+            f"{play_url}&mindate={last_date[str(game_id)]}"
+        ).read()
 
         soup = BeautifulSoup(url_data, "html.parser")
         max_pages = math.ceil(int(soup.plays["total"]) / 100)
 
         for page in tqdm(range(max_pages, 0, -1), maxinterval=max_pages, desc="Page"):
-            url_data = urllib.request.urlopen(f"{play_url}&mindate={last_date[game_id]}&page={page}").read()
+            url_data = urllib.request.urlopen(
+                f"{play_url}&mindate={last_date[str(game_id)]}&page={page}"
+            ).read()
             soup2 = BeautifulSoup(url_data, "html.parser")
             for play in soup2.findAll("play"):
                 if not int(play["id"]) in id_collected:
                     dict = {
-                        "playid": play["id"],
-                        "userid": play["userid"],
+                        "playid": int(play["id"]),
+                        "userid": int(play["userid"]),
                         "date": play["date"],
                         "quantity": play["quantity"],
-                        "length": play["length"],
+                        "length": int(play["length"]),
                         "location": play["location"],
                         "game": play.item["name"],
                     }
@@ -80,26 +83,32 @@ class BGGAPI:
                     if not play.find("players") == None:
                         player_number = 1
                         for player in play.players.findAll("player"):
+                            dict.update(
+                                {
+                                    f"player_{player_number}_team": player["color"],
+                                }
+                            )
                             try:
                                 dict.update(
-                                    {f"player_{player_number}_score": int(player["score"])}
+                                    {
+                                        f"player_{player_number}_score": int(
+                                            player["score"]
+                                        ),
+                                    }
                                 )
                             except:
-                                player_number += 1
                                 pass
                             player_number += 1
                     final_data.append(dict)
-                    last_date.update({game_id: play["date"]})
+                    last_date.update({str(game_id): play["date"]})
                     id_collected.append(int(play["id"]))
-            sleep(1.5)
             with data_export.open("w") as file:
                 json.dump(final_data, file)
-
             id_collected_json.update({game_id: id_collected})
             with id_collected_file.open("w") as file:
                 json.dump(id_collected_json, file)
-
             with last_date_file.open("w") as file:
                 json.dump(last_date, file)
 
+            sleep(1.5)
         return final_data
