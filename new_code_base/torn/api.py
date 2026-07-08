d9229d88f0e5338a36316755b47bcbb10b185b3a
Adam.Borlase <Adam.Borlase@storiogroup.com>
2026-07-08 11:18:01 +0100

    Add start of Torn API

---
+++ new_code_base/torn/api.py
@@ -0,0 +1,11 @@
+from app.API.api import API
+
+class TornAPI(API):
+
+    def __init__(self, key: str, category:str ,params: dict = None):
+
+        base_address = f"https://api.torn.com/v2/{category}/"
+        header = {"accept": "application/json", "Authorization": f"ApiKey {key}"}
+
+        super().__init__(base_address, params, header)
+
