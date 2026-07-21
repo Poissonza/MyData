f48723a3c8d1de3220eaf1977e3356bfe1979758
Poissonza <Poissonza>
2026-07-21 14:21:06 +0000

    Change Storage to fix spark issue

--- new_code_base/DBTools/storage.py
+++ new_code_base/DBTools/storage.py
@@ -3,19 +3,19 @@ from pyspark.sql.functions import from_json, col
 
 class storage:
 
-    def __init__(self):
-        pass
+    def __init__(self, spark):
+        self.spark = spark
 
     def store(self, data: dict, volume_path: str):
 
         json_str = json.dumps(data)
 
-        df_strings = spark.createDataFrame([(json_str,)], ["json_str"])
+        df_strings = self.spark.createDataFrame([(json_str,)], ["json_str"])
 
         schema = df_strings.selectExpr("schema_of_json_agg(json_str)").collect()[0][0]
 
         data_df = df_strings.select(from_json(col("json_str"), schema).alias("parsed")).select("parsed.*")
 
-        data_df.write.format("delta").mode("overwrite").save(f"/Volumes/{volume_path}")
+        data_df.write.format("delta").mode("append").save(f"/Volumes/{volume_path}")
 
         print("data Written")
