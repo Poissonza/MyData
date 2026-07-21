c8b633c1c9cda0a679b19bc92dba3dfc0bb587a1
Poissonza <Poissonza>
2026-07-21 14:22:57 +0000

    Update Faction Model to include all columns

--- new_dbt/models/torn/faction/faction_attacks_test.sql
+++ new_dbt/models/torn/faction/faction_attacks_test.sql
@@ -1,6 +1,19 @@
 SELECT
-    attack.attacker.faction.id as attacker_faction_id,
-    attack.attacker.faction.name as attacker_faction_name
+    attack.id as id,
+    attack.code as code,
+    attack.started as started,
+    attack.ended as ended,
+    attack.result as result,
+    attack.attacker as attacker,
+    attack.defender as defender,
+    attack.respect_gain as respect_gain,
+    attack.respect_loss as respect_loss,
+    attack.chain as chain,
+    attack.is_interrupted as is_interrupted,
+    attack.is_raid as is_raid,
+    attack.is_ranked_war as is_ranked_war,
+    attack.is_stealthed as is_stealthed,
+    attack.modifiers as modifiers
 FROM (
   SELECT explode(attacks) as attack
   FROM delta.`/Volumes/torn/faction/faction_api_files/faction_attacks/`
