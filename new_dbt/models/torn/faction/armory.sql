b73718f5344f828023f7192a4fcab00b0494edff
Poissonza <Poissonza>
2026-07-28 10:51:49 +0000

    Add Armory News to data collection

---
+++ new_dbt/models/torn/faction/armory.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_id = 'id',
+        alias ='faction_armory'
+    )
+}}
+SELECT news.id AS id,
+news.text as news_text,
+news.timestamp as timestamp
+FROM (
+SELECT EXPLODE(news) as news FROM delta.`/Volumes/torn/faction/faction_api_files/news`
+)
+{% if is_incremental() %}
+WHERE timestamp > (SELECT max(timestamp) FROM {{this}})
+{% endif %}
